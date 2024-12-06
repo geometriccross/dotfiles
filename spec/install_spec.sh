e76cf7c62e0a1930074b94ac30d0129d059f7075
geometriccross <geometriccross@gmail.com>
2024-12-06 22:42:29 +0900

    add test case

--- spec/install_spec.sh
+++ spec/install_spec.sh
@@ -5,6 +5,11 @@ Describe 'filtering_path test'
         When call filtering_path hoge
         The output should equal hoge 
     End
+
+    It 'wrong param'
+        When call filtering_path ".gitignore"
+        The status should be failure
+    End
 End
 
 Describe 'create_link test'
