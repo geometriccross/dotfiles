2b3d9325e60edf1474193c5e2da435e7b8d2bcb7
geometriccross <geometriccross@LAPTOP.localdomain>
2024-12-06 20:55:41 +0900

    modified .gitignore to recongination spec dir

---
+++ spec/install_spec.sh
@@ -0,0 +1,9 @@
+Describe 'install.sh function test'
+    Include ./install.sh
+
+    
+    It 'check filtering_path can filter path'
+        When call filtering_path hoge
+        The output should equal hoge 
+    End
+End
