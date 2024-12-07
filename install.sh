b068d88810478abbc913c0fa4e14d5dffda6b219
geometriccross <geometriccross@gmail.com>
2024-12-07 20:09:04 +0900

    fix script to refer base_dir when create link

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-ls -aF $(cd $(dirname ${BASH_SOURCE:-$0}); pwd) \
+base_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+
+ls -aF ${base_dir} \
 	| grep -v ".gitignore" \
 	| grep -v "install.sh" \
 	| grep -v / \
-	| xargs -I FILE ln -s FILE ${1:-${HOME}}/FILE 
+	| xargs -I FILE ln -s ${base_dir}/FILE ${1:-${HOME}}/FILE 
