c8c903373c1326da9b834283285fe3e382e9337d
geometriccross <geometriccross@gmail.com>
2024-12-07 19:53:04 +0900

    fix bugs

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-dest=${1:=~}
-shift
-
-ls -aF . \
+ls -aF $(cd $(dirname ${BASH_SOURCE:-$0}); pwd) \
 	| grep -v ".gitignore" \
 	| grep -v "install.sh" \
 	| grep -v / \
-	| xargs -I FILE ln -s FILE ${dest}/FILE 
+	| xargs -I FILE ln -s FILE ${1:-${HOME}}/FILE 
