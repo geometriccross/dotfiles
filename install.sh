ab1ceb4e75ec92be57965a94ad144251117b9528
geometriccross <geometriccross@gmail.com>
2024-12-10 23:24:55 +0900

    add test for filtering path

--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-base_dir=$(cd "$(dirname "${BASH_SOURCE:-$0}")" || "${HOME}"; pwd)
+filtering_path() {
+	for file in ./.*
+	do
+		case "${file}" in
+			./.git*) ;;
+			./.shellspec) ;;
+			*) echo "${file}" 
+		esac
+	done
+}
 
-for file in ./.*
-do
-	case "${file}" in
-		./.git*) ;;
-		./.shellspec) ;;
-		*) echo ln -s "${base_dir}/FILE" "${1:-${HOME}}/FILE" ;;
-	esac
-done
+main() {
+	base_dir=$(cd "$(dirname "${BASH_SOURCE:-$0}")" || "${HOME}"; pwd)
+	ln -s "${base_dir}/FILE" "${1:-${HOME}}/FILE"
+}
 
--- spec/install_spec.sh
+++ spec/install_spec.sh
@@ -1,3 +1,10 @@
 Describe 'install.sh test'
   Include ./install.sh
+
+  Context 'filtering path'
+	It 'filtering path'
+	  When call filtering_path  
+	  The stdout should not include "./.git" 
+	End
+  End
 End
