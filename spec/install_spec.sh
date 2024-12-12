8c3f2ad4872610e00ccdd4932bf9239b44af77cc
geometriccross <geometriccross@LAPTOP.localdomain>
2024-12-12 10:57:06 +0900

    change a format and describe sentences

--- spec/install_spec.sh
+++ spec/install_spec.sh
@@ -1,22 +1,25 @@
-Describe 'install.sh test'
-  Include ./install.sh
+Include ./install.sh
 
-  Context 'filtering path'
-	It 'filtering path'
-	  When call filtering_path  
-	  The stdout should not include "./.git" 
+Describe 'filtering_path test'
+	Context 'when call filtering_path'
+		It 'filtering path'
+			When call filtering_path  
+			The stdout should not include "./.git" 
+		End
 	End
-  End
-
-  Context 'process_env_is'
-	It 'can return wsl' 
-	  When call process_env_is	
-	  The stdout should equal "wsl"	
-	End
-
-	It 'can return container'
-	  When call docker run --rm -v "$(pwd)/install.sh:/install.sh" alpine sh -c ". install.sh; process_env_is"
-	  The stdout should equal "container"
-	End
-  End
 End
+
+Describe 'process_env_is test'
+	Context 'when call process_env_is'
+		It 'can return wsl' 
+			When call process_env_is	
+			The stdout should equal "wsl"	
+		End
+
+		It 'can return container'
+			When call docker run --rm -v "$(pwd)/install.sh:/install.sh" alpine sh -c ". install.sh; process_env_is"
+			The stdout should equal "container"
+		End
+	End
+End
+
