eba4b6830a907069191ee9f82b6496d8e694cda6
geometriccross <geometriccross@LAPTOP.localdomain>
2024-12-20 16:41:44 +0900

    replace install cmds and format scripts

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ process_env_is() {
 
 sand_with_bar() {
 	# between \e[ and color number is necessary to keep space
-	echo =================== "${*}" ===================  
+	echo =================== "${*}" ===================
 }
 
 check_cmd_with_prompt() {
@@ -49,52 +49,39 @@ install_with_prompt() {
 install_wizard() {
 	app_name="${1}" && shift
 	check_cmd="${1}" && shift
-	
+
 	# change a color into blue
 	printf "\e[34m"
 	sand_with_bar "${app_name}"
 
-	check_cmd_with_prompt "${app_name}" "${check_cmd}" \
-		&& install_with_prompt "${app_name}" "${@}"
+	check_cmd_with_prompt "${app_name}" "${check_cmd}" &&
+		install_with_prompt "${app_name}" "${@}"
 	printf "\e[0m"
 }
 
-# ---------- Setup Editor ----------
-install_nvim() {
-	if ! nvim -v >/dev/null; then
-		echo Neovim is not installed.
-		echo start install...
-
-		curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage &&
-			sudo chmod u+x nvim.appimage &&
-			sudo ./nvim.appimage &&
-			echo Install Neovim success!
-
-		echo Install required packages...
-		sudo apt install -y make gcc ripgrep unzip git xclip &&
-			echo Install required packages success!
-	else
-		echo Neovim is already installed.
-	fi
-}
-
-install_kickstart() {
-	local path="${HOME}/.config/nvim"
-	if [[ ! -e $path ]]; then
-		echo kickstart.vim is not installed.
-		echo Start install kickstart.nvim
-
-		git clone git@github.com:geometriccross/kickstart.nvim.git "${path}" &&
-			echo Install kickstart.vim success!
-	else
-		echo kickstart.vim is already installed.
-	fi
-}
 main() {
 	base_dir=$(
 		cd "$(dirname "${BASH_SOURCE:-$0}")" || "${HOME}"
 		pwd
 	)
+
+	# install neovim and required packages
+	install_wizard "Neovim" "nvim -v" "$(
+		cat <<-EOF
+			curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+				&& sudo chmod u+x nvim.appimage
+				&& sudo ./nvim.appimage
+
+			sudo apt install -y make gcc ripgrep unzip git xclip
+		EOF
+	)"
+
+	install_wizard "Neovim settings" "test -d $HOME/config/nvim" "$(
+		cat <<-EOF
+			git clone https://github.com/geometriccross/nvim_settings.git $HOME/.config/nvim
+		EOF
+	)"
+
 	case $(process_env_is) in
 	container)
 		# Default powerline10k theme, no plugins installed
@@ -107,4 +94,3 @@ main() {
 
 	ln -s "${base_dir}/FILE" "${1:-${HOME}}/FILE"
 }
-
