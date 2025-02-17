9b563c9e05e7b5ced482501961ff85e0ac0cd842
geometriccross <geometriccross@gmail.com>
2025-02-18 01:23:30 +0900

    add installer finally

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y ansible git
+
+git clone https://github.com/geometriccross/dotfiles.git ~/.dotfiles
+ansible-playbook -K ~/.dotfiles/playbook.yml --extra-vars "user=$(whoami)"
+
+sudo apt remove ansible
+sudo apt autoremove
