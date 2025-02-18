6024a2b1665f24acf6dab968eed0216af50e44da
geometriccross <geometriccross@gmail.com>
2025-02-18 16:30:10 +0900

    add description

--- modules/push.sh
+++ modules/push.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# create directory that contain foward reverse fastq pair
+# directory name are decided with file name
 find "${1:-.}" -maxdepth 1 -name "*.fastq.gz" | while read -r file; do
 	dir_name=$(echo "${file}" | sed 's/_L001_R._001.fastq.gz//g' | xargs -0 basename -a)
 	mkdir -p "${dir_name}"
