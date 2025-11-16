fbf88dfa65406465f2133ac16f28f6a0245be741
geometriccross <geometriccross@desktop.>
2025-11-16 21:28:11 +0900

    change the log order

--- install.sh
+++ install.sh
@@ -25,8 +25,8 @@ log_error() {
 
 # --- install common packages -------------------------------
 log_info "Installing common packages..." &&
-	sudo apt-get update &&
-	sudo apt-get install -y \
+	apt-get update &&
+	apt-get install -y \
 		zsh \
 		wget \
 		curl \
@@ -35,28 +35,28 @@ log_info "Installing common packages..." &&
 
 # --- System Setup -------------------------------
 # shell change
-grep -q "^/usr/bin/zsh$" /etc/shells ||
-	log_info "Adding zsh to /etc/shells..." &&
-	echo "/usr/bin/zsh" | sudo tee -a /etc/shells >/dev/null
+log_info "Adding zsh to /etc/shells..." &&
+	grep -q "^/usr/bin/zsh$" /etc/shells ||
+	echo "/usr/bin/zsh" | tee -a /etc/shells >/dev/null
 
-[[ "$SHELL" == *"zsh"* ]] ||
-	log_info "Changing default shell to zsh..." &&
+log_info "Changing default shell to zsh..." &&
+	[[ "$SHELL" == *"zsh"* ]] ||
 	chsh -s /usr/bin/zsh
 
-[[ -e $HOME/.zplug ]] ||
-	curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh &&
-	log_info "Installing zplug..."
+log_info "Installing zplug..." &&
+	[[ -e $HOME/.zplug ]] ||
+	curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
 # chell change
 SUDOERS_FILE="/etc/sudoers.d/$USER"
-[ -f "$SUDOERS_FILE" ] ||
-	log_info "Adding user to sudoers..." &&
-	echo "$USER    ALL=NOPASSWD: ALL" | sudo tee "$SUDOERS_FILE" >/dev/null &&
-	sudo chmod 440 "$SUDOERS_FILE"
+log_info "Adding user to sudoers..." &&
+	[ -f "$SUDOERS_FILE" ] ||
+	echo "$USER    ALL=NOPASSWD: ALL" | tee "$SUDOERS_FILE" >/dev/null &&
+	chmod 440 "$SUDOERS_FILE"
 
 # --- Dotfiles -------------------------------
-[ -d "$DOTFILES_DIR" ] ||
-	log_info "Cloning dotfiles repository..." &&
+log_info "Cloning dotfiles repository..." &&
+	[ -d "$DOTFILES_DIR" ] ||
 	git clone https://github.com/geometriccross/dotfiles.git "$DOTFILES_DIR"
 
 find "$DOTFILES_DIR"/dot -name ".*" -type f | while read -r file; do
@@ -67,8 +67,8 @@ find "$DOTFILES_DIR"/dot -name ".*" -type f | while read -r file; do
 done
 
 # --- install aqua packages -------------------------------
-[[ -e $HOME/.local/share/aquaproj-aqua/bin/aqua ]] ||
-	log_info "Installing aqua..." &&
+log_info "Installing aqua..." &&
+	[[ -e $HOME/.local/share/aquaproj-aqua/bin/aqua ]] ||
 	curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v4.0.2/aqua-installer | bash
 
 aqua i -a
