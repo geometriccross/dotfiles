894de84602f19bdd21d7ca04d27d267d0ca13758
geometriccross <geometriccross@desktop.>
2025-11-16 23:54:30 +0900

    Add container installing script

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 ## Easy Install
 ```
-sudo bash <(curl https://raw.githubusercontent.com/geometriccross/dotfiles/refs/heads/main/install.sh)
+bash <(curl https://raw.githubusercontent.com/geometriccross/dotfiles/refs/heads/main/install.sh)
+```
+
+## For Container (Non Root Only)
+```
+curl -fsSL https://raw.githubusercontent.com/geometriccross/dotfiles/refs/heads/main/container.sh | bash
 ```
--- container.sh
+++ container.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-USER=$(whoami)
-DOTFILES_DIR="$HOME/.dotfiles"
-
 # 色付き出力用
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
@@ -23,31 +20,7 @@ log_error() {
 	echo -e "${RED}[ERROR]${NC} $1"
 }
 
-# --- install common packages -------------------------------
-log_info "Installing common packages..." &&
-	sudo apt-get update &&
-	sudo apt-get install -y \
-		zsh \
-		zplug \
-		wget \
-		curl \
-		git \
-		locales-all
-
-# --- System Setup -------------------------------
-log_info "Adding zsh to /etc/shells..." &&
-	grep -q "^/usr/bin/zsh$" /etc/shells ||
-	echo "/usr/bin/zsh" | tee -a /etc/shells >/dev/null
-
-log_info "Changing default shell to zsh..." &&
-	[[ "$SHELL" == *"zsh"* ]] ||
-	sudo chsh -s /usr/bin/zsh
-
-SUDOERS_FILE="/etc/sudoers.d/$USER"
-log_info "Adding user to sudoers..." &&
-	[ -f "$SUDOERS_FILE" ] ||
-	echo "$USER    ALL=NOPASSWD: ALL" | tee "$SUDOERS_FILE" >/dev/null &&
-	chmod 440 "$SUDOERS_FILE"
+DOTFILES_DIR="$HOME/.dotfiles"
 
 # --- Dotfiles -------------------------------
 log_info "Cloning dotfiles repository..." &&
@@ -62,5 +35,3 @@ ln -sf "$DOTFILES_DIR"/dot/.zsh.d "$HOME/.zsh.d" && log_info "Created symlink: $
 log_info "Installing aqua..." &&
 	[[ -e $HOME/.local/share/aquaproj-aqua/bin/aqua ]] ||
 	curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v4.0.2/aqua-installer | bash
-
-aqua i -a
