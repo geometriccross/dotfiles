91858b79a7ccb7e8ac05f916b0c8a76a02b7d796
geometriccross <geometriccross@desktop.>
2025-11-16 15:23:55 +0900

    add alias

--- dot/.zsh.d/alias.zsh
+++ dot/.zsh.d/alias.zsh
@@ -4,4 +4,17 @@ alias v="nvim"
 alias vi="nvim"
 alias vim="nvim"
 
+alias ll="ls -alF"
+alias la="ls -A"
+alias l="ls -CF"
 
+alias f="find . -name"
+
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit"
+alias gp="git push"
+alias gl="git log"
+
+alias dup="devcontainer up --workspace-folder ."
+alias dexec="devcontainer exec --workspace-folder ."
