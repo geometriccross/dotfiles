49ea7073cca8a00920e511161124fb0dd21bc465
geometriccross <geometriccross@gmail.com>
2026-07-09 11:52:59 +0900

    change pick project module path spec

--- modules/pick-project.sh
+++ modules/pick-project.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env zsh
 
 function peco-projects () {
-  local selected_dir=$(ghq list -p | peco --query "$LBUFFER")
+  local selected_dir=$(ls -1 ~/projects | peco --query "$LBUFFER")
   if [ -n "$selected_dir" ]; then
-    BUFFER="cd ${selected_dir}"
+    BUFFER="cd ~/projects/${selected_dir}"
     zle accept-line
   fi
   zle clear-screen
