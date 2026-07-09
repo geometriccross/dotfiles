8290e545c8543ca81e44500d0cd14b196a64aed8
geometriccross <geometriccross@gmail.com>
2026-07-09 11:44:11 +0900

    delete unuse auto cmd

--- lua/config/common/autocmd.lua
+++ lua/config/common/autocmd.lua
@@ -7,38 +7,3 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
-
---- Check if a file or directory exists in this path
-local function exists(file)
-	local ok, err, code = os.rename(file, file)
-	if not ok then
-		if code == 13 then
-			-- Permission denied, but it exists
-			return true
-		end
-	end
-	return ok, err
-end
-
---- Check if a directory exists in this path
-local function isdir(path)
-	-- "/" works on both Unix and Windows
-	return exists(path .. "/")
-end
-
-vim.api.nvim_create_autocmd("BufWritePost", {
-	desc = "run shellspec if spec folder exists",
-	pattern = "*.sh",
-	callback = function()
-		-- check current directory has spec folder
-		if isdir(vim.fn.getcwd() .. "/spec") then
-			if not term:is_open() then
-				term:open()
-				term:resize(term_size)
-			end
-
-			term:clear()
-			term:send("shellspec")
-		end
-	end,
-})
