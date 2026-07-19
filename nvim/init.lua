4baebf657aea6fd2dfe505a2cbfd4c8e046d39f1
geometriccross <geometriccross@gmail.com>
2026-07-19 17:54:26 +0900

    refactor nvim config layout

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,26 +1,14 @@
 -- plugin import ---------------------------------
 require("config.lazy")
-local plugins = {
-	{ import = "config.common.plugins" },
+SetupLazy({
+	{ import = "plugins" },
 	{ import = "colorscheme" },
-}
-if vim.g.vscode then
-	-- table.insert(plugins, { import = "config.vscode.plugins" })
-else
-	table.insert(plugins, { import = "config.neovim.plugins" })
-end
-SetupLazy(plugins)
+})
 
 -- setting import ---------------------------------
-require("config.common.autocmd")
-require("config.common.keymaps")
-require("config.common.settings")
-
-if vim.g.vscode then
-	-- vscode specific settings
-else
-	require("config.neovim.autocmd")
-	require("config.neovim.lsp")
-end
+require("config.autocmds")
+require("config.keymaps")
+require("config.options")
+require("config.lsp")
 
 vim.cmd [[colorscheme tokyonight]]
